8de63bebc7c1456829bcb857d111d3bebb8c5079
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2025-05-04 10:26:45 +0200

    improve exec.php
    v2

--- exec.php
+++ exec.php
@@ -1,35 +1,40 @@
 <?php
 if (
-    !file_exists('bin/obfuscate.php') ||
-    !file_exists('bin/colors.php') ||
-    !file_exists('bin/header.php')
+    !file_exists('.bin/obfuscate.php') ||
+    !file_exists('.bin/colors.php') ||
+    !file_exists('.bin/header.php')
 ) {
     echo "\nSome Files Missing, Please Download The Tool Again\nYou Can Download it With This Command:\n";
     echo "git clone https://github.com/serainox420/Blind-Bash-v2\n\n";
     exit(0);
 } else {
-    require('bin/obfuscate.php');
-    require('bin/colors.php');
-    require('bin/header.php');
+    require('.bin/obfuscate.php');
+    require('.bin/colors.php');
+    require('.bin/header.php');
+}
+
+if ($argc !== 3) {
+    echo "Usage: php exec.php <file> <repeat_count>\n";
+    exit(1);
+}
+
+$filename = trim($argv[1]);
+$repeatCount = (int)$argv[2];
+
+if (!file_exists($filename)) {
+    echo "$filename does not exist.\n";
+    exit(1);
+}
+
+if ($repeatCount <= 0) {
+    echo "Repeat count must be a positive integer.\n";
+    exit(1);
 }
 
 head();
-$filename = readline(" > Enter Input File Name: ");
-$filename = trim($filename);
-
-if (!file_exists($filename)) {
-    echo "$filename Does Not Exist\n";
-    exit(1);
-}
 
 $initialSize = filesize($filename);
 $initialContents = file_get_contents($filename);
-
-$repeatCount = (int)readline("  ~ Enter Number of Repeats: ");
-if ($repeatCount <= 0) {
-    echo "Invalid repeat count\n";
-    exit(1);
-}
 
 for ($i = 0; $i < $repeatCount; $i++) {
     $out = dirname(__FILE__) . DIRECTORY_SEPARATOR . $filename;
@@ -43,4 +48,3 @@ $processedContents = file_get_contents($filename);
 echo "\nInitial Size: $initialSize bytes, Characters: " . strlen($initialContents) . PHP_EOL;
 echo "Processed Size: $processedSize bytes, Characters: " . strlen($processedContents) . PHP_EOL;
 echo "Obfuscation process completed.\n";
-?>
