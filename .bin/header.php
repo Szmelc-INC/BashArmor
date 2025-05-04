73cd192b86bb57fabba4b5b43641641f7c280fe0
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2025-05-04 10:26:17 +0200

    Add files via upload

---
+++ .bin/header.php
@@ -0,0 +1,24 @@
+<?php
+	function head(){
+		require('colors.php');
+		system('clear');
+echo "\n";		
+echo " ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+echo " ┃=-=========================================================-=┃\n";
+echo " ┃  ____            _        ___                               ┃\n";
+echo " ┃ | __ )  __ _ ___| |__    / / \   _ __ _ __ ___   ___  _ __  ┃\n";
+echo " ┃ |  _ \ / _` / __| '_ \  / / _ \ | '__| '_ ` _ \ / _ \| '__| ┃\n";
+echo " ┃ | |_) | (_| \__ \ | | |/ / ___ \| |  | | | | | | (_) | |    ┃\n";
+echo " ┃ |____/ \__,_|___/_| |_/_/_/   \_\_|  |_| |_| |_|\___/|_|v2  ┃\n";
+echo " ┃                                                             ┃\n";
+echo " ┃=-=========================================================-=┃\n";
+echo " ┃                                                             ┃\n";
+echo " ┃ [ BlindShell fork by SX ] : [Szmelc.INC]                    ┃\n";
+echo " ┃ > Improved Bash Obfuscator written in .php                  ┃\n";
+echo " ┃ > [Discord: sx66] / https://szmelc.com/discord              ┃\n";
+echo " ┃ > https://github.com/Szmelc-INC/BashArmor                   ┃\n";
+echo " ┃                                                             ┃\n";
+echo " ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n$RR";
+echo "\n";
+	}
+?>
